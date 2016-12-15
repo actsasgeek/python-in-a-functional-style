63ea0ab31c909bdab36b4dbccf6741130a4ba57b
Steve Butcher <actsasgeek@gmail.com>
2016-12-15 12:32:52 -0500

    Make it so

---
+++ multimethods.py
@@ -0,0 +1,21 @@
+def multi(dispatch_fn):
+    def _inner(*args, **kwargs):
+        return _inner.__multi__.get(
+            dispatch_fn(*args, **kwargs),
+            _inner.__multi_default__
+        )(*args, **kwargs)
+
+    _inner.__multi__ = {}
+    _inner.__multi_default__ = lambda *args, **kwargs: None  # Default default
+    return _inner
+
+def method(dispatch_fn, dispatch_key=None):
+    def apply_decorator(fn):
+        if dispatch_key is None:
+            # Default case
+            dispatch_fn.__multi_default__ = fn
+        else:
+
+            dispatch_fn.__multi__[dispatch_key] = fn
+        return dispatch_fn
+    return apply_decorator
